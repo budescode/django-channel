95e47857ea9781226671823733849a24bb1cf4db
Omonbude Emmanuel <gospeltruth18@gmail.com>
2020-06-09 19:00:58 +0100

    heroku

--- cfehome/settings.py
+++ cfehome/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'em-9$ln6^a0z!s2pbo=mu*l$cgnqgsyd_z21f-%2d(_h7*wu^0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
