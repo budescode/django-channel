cca743a6b3d6aab9b4e3ced034135312e8ff8d0d
Omonbude Emmanuel <gospeltruth18@gmail.com>
2020-06-09 19:21:18 +0100

    heroku bug fixed

--- cfehome/settings.py
+++ cfehome/settings.py
@@ -13,6 +13,7 @@ https://data.heroku.com/datastores/2191e023-7cc5-4dc0-b058-5396a8951330#
 
 import os
 import dj_database_url
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -150,3 +151,5 @@ CHANNEL_LAYERS = {
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+
+django_heroku.settings(locals())
